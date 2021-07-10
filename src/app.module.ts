db3d8a14113a9cbd6364d80f03b3ccb78c34085c
Gabriel <depelos_07_@hotmail.com>
2021-07-10 07:07:13 -0500

    todo bien

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+import config from './config';
+import { enviroments } from './enviroments';
+import { DatabaseModule } from './database/database.module';
+
+@Module( {
+  imports: [
+    ConfigModule.forRoot( {
+      envFilePath: enviroments[ process.env.NODE_ENV ] || '.env',
+      load: [ config ],
+      isGlobal: true
+    } ),
+    DatabaseModule
+  ],
+  controllers: [ AppController ],
+  providers: [ AppService ],
+} )
+export class AppModule { }
---
+++ src/database/database.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ConfigType } from '@nestjs/config';
+import config from 'src/config';
+
+@Module( {
+  imports: [
+    TypeOrmModule.forRootAsync( {
+      inject: [ config.KEY ],
+      useFactory: ( configService: ConfigType<typeof config> ) => {
+        // const { user, host, dbName, password, port } = configService.postgres;
+        return {
+          type: 'postgres',
+          url: configService.postgresUrl,
+          synchronize: false,
+          autoLoadEntities: true,
+          //ssl: { rejectUnauthorized: false }
+        };
+      },
+    } )
+  ],
+  exports: [ TypeOrmModule ]
+} )
+export class DatabaseModule { }
